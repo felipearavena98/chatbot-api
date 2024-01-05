d5e331e87b0f095cc62cfc51740906305fc2a004
Felipe <felipearavena98@hotmail.com>
2024-01-05 18:08:06 -0300

    update verify

--- netlify/functions/src/controllers/whatsappControllers.js
+++ netlify/functions/src/controllers/whatsappControllers.js
@@ -1,5 +1,21 @@
 const VerifyToken = (req, res) => {
-    res.send('Hola verifyToken');
+    try {
+        let accessToken = 'ASDASDASSADSAD';
+        let token = req.query["hub.verify_token"];
+        let challenge = req.body["hub.challenge"];
+
+        if(challenge !== null && token != null && token == accessToken) {
+            res.send(challenge)
+        } else {
+            res.status(400).send();
+        }
+
+    } catch (e) {
+        res.status(400).send()
+    }
+
+
+    // res.send('Hola verifyToken');
 }
 
 const ReceivedMessage = (req,res) => {
