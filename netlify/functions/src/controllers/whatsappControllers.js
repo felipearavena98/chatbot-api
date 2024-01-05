caef672ff85f9cd964dc0bd40f5ca80db91032a8
Felipe <felipearavena98@hotmail.com>
2024-01-05 18:20:10 -0300

    update verify 3

--- netlify/functions/src/controllers/whatsappControllers.js
+++ netlify/functions/src/controllers/whatsappControllers.js
@@ -2,7 +2,7 @@ const VerifyToken = (req, res) => {
     try {
         let accessToken = 'ASDASDASSADSAD';
         let token = req.query["hub.verify_token"];
-        let challenge = req.body["hub.challenge"];
+        let challenge = req.query["hub.challenge"];
 
         if(challenge != null && token != null && token == accessToken) {
             res.send(challenge)
