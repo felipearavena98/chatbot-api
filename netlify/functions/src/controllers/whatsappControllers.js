ccc04f5ac40546eed03acd937ff2807065c2ffb4
Felipe <felipearavena98@hotmail.com>
2024-01-05 19:41:26 -0300

    update received 3

--- netlify/functions/src/controllers/whatsappControllers.js
+++ netlify/functions/src/controllers/whatsappControllers.js
@@ -1,6 +1,10 @@
 const fs = require("fs");
-const myConsole = new console.Console(fs.createWriteStream("./logs.txt"));
+const path = require("path");
 
+// Utiliza el directorio /tmp para los logs en el entorno serverless
+const logFilePath = path.join('/tmp', 'logs.txt');
+const logStream = fs.createWriteStream(logFilePath, { flags: 'a' });
+const myConsole = new console.Console(logStream);
 
 const VerifyToken = (req, res) => {
     try {
@@ -9,24 +13,21 @@ const VerifyToken = (req, res) => {
         let challenge = req.query["hub.challenge"];
 
         if(challenge != null && token != null && token == accessToken) {
-            res.send(challenge)
+            res.send(challenge);
         } else {
             res.status(400).send();
         }
 
     } catch (e) {
-        res.status(400).send()
+        myConsole.log(e);
+        res.status(400).send();
     }
+};
 
-
-    // res.send('Hola verifyToken');
-}
-
-const ReceivedMessage = (req,res) => {
+const ReceivedMessage = (req, res) => {
     try {
-
-        let entry = (req.body["entry"])[0];
-        let changes = (entry["changes"])[0];
+        let entry = req.body["entry"][0];
+        let changes = entry["changes"][0];
         let value = changes["value"];
         let messageObject = value["messages"];
 
@@ -37,10 +38,9 @@ const ReceivedMessage = (req,res) => {
         myConsole.log(e);
         res.send("EVENT_RECEIVED");
     }
-    // res.send('Hola Received');
-}
+};
 
 module.exports = {
     VerifyToken,
     ReceivedMessage
-}
+};
