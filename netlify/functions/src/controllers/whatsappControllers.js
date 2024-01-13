6e9993d945396a121cd1617f07008dd6977fcd36
Felipe <felipearavena98@hotmail.com>
2024-01-13 01:37:22 -0300

    whatsappServices 10

--- netlify/functions/src/controllers/whatsappControllers.js
+++ netlify/functions/src/controllers/whatsappControllers.js
@@ -35,7 +35,7 @@ const ReceivedMessage = (req, res) => {
         if(typeof messageObject != "undefined") {
             let messages = messageObject[0];
             let phoneNumber = messages["from"];
-            phoneNumber = phoneNumber.replace('569','');
+            phoneNumber = phoneNumber.replace('569','+569');
             console.log(phoneNumber)
             let text = GetTextUser(messages);
             
