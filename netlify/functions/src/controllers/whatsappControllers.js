469bc3eb9fd82940b484698068137307fb356573
Felipe <felipearavena98@hotmail.com>
2024-01-13 01:42:58 -0300

    whatsappServices 11

--- netlify/functions/src/controllers/whatsappControllers.js
+++ netlify/functions/src/controllers/whatsappControllers.js
@@ -35,8 +35,8 @@ const ReceivedMessage = (req, res) => {
         if(typeof messageObject != "undefined") {
             let messages = messageObject[0];
             let phoneNumber = messages["from"];
-            phoneNumber = phoneNumber.replace('569','+569');
-            console.log(phoneNumber)
+            // phoneNumber = phoneNumber.replace('569','');
+            // console.log(phoneNumber)
             let text = GetTextUser(messages);
             
 
--- netlify/functions/src/services/whatsappService.js
+++ netlify/functions/src/services/whatsappService.js
@@ -1,43 +1,43 @@
-const https = require("https");
+    const https = require("https");
 
-function SendMessageWhatsApp(textResponse, number){
-    const data = JSON.stringify({
-        "messaging_product": "whatsapp",
-        "to": number,
-        "text": {
-            "body": textResponse
-        },
-        "type": "text"
-    })
-console.log('Paso 1')
-    const options = {
-        host: "graph.facebook.com",
-        path: "/v17.0/166650459868747/messages",
-        method: "POST",
-        body: data,
-        headers: {
-            "Content-Type": "application/json",
-            Authorization: "Bearer EAAMYnEKWwo4BOzF0rS9EbN6XUENt11Cmr1q5DcFFaYz4eznjJ9jdRFuOOqyMKSfgxhVrwnF0Nm1QlMh2C2X4EH5342S3sMB0xahZBLeUR9aq84P5Q1TIhMxOVzLKaMTWPh4kSZAcTPIEyWQwGbonWvZBZAxSFxQ0DYMGBQzSDZBmER1whpIzOxo2nXjrZBzsxB"
+    function SendMessageWhatsApp(textResponse, number){
+        const data = JSON.stringify({
+            "messaging_product": "whatsapp",
+            "to": number,
+            "text": {
+                "body": textResponse
+            },
+            "type": "text"
+        })
+    console.log('Paso 1')
+        const options = {
+            host: "graph.facebook.com",
+            path: "/v18.0/166650459868747/messages",
+            method: "POST",
+            body: data,
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: "Bearer EAAMYnEKWwo4BOzF0rS9EbN6XUENt11Cmr1q5DcFFaYz4eznjJ9jdRFuOOqyMKSfgxhVrwnF0Nm1QlMh2C2X4EH5342S3sMB0xahZBLeUR9aq84P5Q1TIhMxOVzLKaMTWPh4kSZAcTPIEyWQwGbonWvZBZAxSFxQ0DYMGBQzSDZBmER1whpIzOxo2nXjrZBzsxB"
+            }
         }
-    }
-    console.log('Paso 2')
-    const req = https.request(options, res => {
-        res.on("data", d=> {
-            process.stdout.write(d);
+        console.log('Paso 2')
+        const req = https.request(options, res => {
+            res.on("data", d=> {
+                process.stdout.write(d);
+            });
         });
-    });
-    console.log('Paso 3')
-    req.on("error", error => {
-        console.error(error);
-    });
-    console.log('Paso 4')
-    req.write(data);
-    console.log('Paso 5')
-    req.end();
-    console.log('Paso 6')
+        console.log('Paso 3')
+        req.on("error", error => {
+            console.error(error);
+        });
+        console.log('Paso 4')
+        req.write(data);
+        console.log('Paso 5')
+        req.end();
+        console.log('Paso 6')
 
-}
+    }
 
-module.exports = {
-    SendMessageWhatsApp
-}
+    module.exports = {
+        SendMessageWhatsApp
+    }
