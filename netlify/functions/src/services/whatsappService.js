e97aaacd40e46b8b0c3a2be5badf8f3359285d56
Felipe <felipearavena98@hotmail.com>
2024-01-14 04:13:43 -0300

    whatsappServices 34

--- netlify/functions/src/services/whatsappService.js
+++ netlify/functions/src/services/whatsappService.js
@@ -1,43 +1,43 @@
-    const https = require("https");
+const https = require("https");
 
-    const SendMessageWhatsApp = async (data) => {
-        try {
-            
-            const resolveData = await data;
+const SendMessageWhatsApp = (data) => {
+    try {
+        const jsonData = JSON.stringify(data);
 
-            const options = {
-                host: "graph.facebook.com",
-                path: "/v17.0/166650459868747/messages",
-                method: "POST",
-                body: data,
-                headers: {
-                    "Content-Type": "application/json",
-                    Authorization: "Bearer EAAMYnEKWwo4BOzF0rS9EbN6XUENt11Cmr1q5DcFFaYz4eznjJ9jdRFuOOqyMKSfgxhVrwnF0Nm1QlMh2C2X4EH5342S3sMB0xahZBLeUR9aq84P5Q1TIhMxOVzLKaMTWPh4kSZAcTPIEyWQwGbonWvZBZAxSFxQ0DYMGBQzSDZBmER1whpIzOxo2nXjrZBzsxB"
-                }
+        const options = {
+            host: "graph.facebook.com",
+            path: "/v17.0/166650459868747/messages",
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: "Bearer TU_TOKEN_DE_ACCESO"
             }
-            console.log('OPTIONS: ', options);
+        };
 
         const req = https.request(options, res => {
-            res.on("data", d => {
-                process.stdout.write(d)
-            })
-        })
+            let responseData = '';
+            res.on("data", chunk => {
+                responseData += chunk;
+            });
+            res.on('end', () => {
+                console.log('Respuesta del servidor:', responseData);
+            });
+        });
 
         req.on("error", error => {
             console.error('Error en la solicitud:', error);
         });
 
-        req.write(resolveData);
+        req.write(jsonData);
         req.end();
 
         console.log('Solicitud enviada');
 
     } catch (error) {
-        console.log('Error en SendMessageWhatsApp:', error);
+        console.error('Error en SendMessageWhatsApp:', error);
     }
-        
-    }
+};
 
-    module.exports = {
-        SendMessageWhatsApp
-    }
+module.exports = {
+    SendMessageWhatsApp
+};
